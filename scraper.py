16b7ee77ab86e80ab62bec0afc50ea88eb634845
mariannediaz <mariannediaz@gmail.com>
2016-07-20 10:14:36 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -14,8 +14,8 @@ LANGUAGE = 'en'
 NUM_PAGES = 15 
 
 for page in range(1, NUM_PAGES+1):
-    base_url = 'http://search.twitter.com/search.json?q=%s&rpp=%s&lang=%s&page=%s' \
-% (urllib2.quote(QUERY), RESULTS_PER_PAGE, LANGUAGE, page)
+        base_url = 'http://search.twitter.com/search.json?q=%s&rpp=%s&lang=%s&page=%s' \
+            % (urllib2.quote(QUERY), RESULTS_PER_PAGE, LANGUAGE, page)
        try:
         results_json = simplejson.loads(scraperwiki.scrape(base_url))
         for result in results_json['results']:
