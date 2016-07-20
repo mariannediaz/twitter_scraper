3af6d9d53c9c4743a4365774a810a7c9afb92c2e
mariannediaz <mariannediaz@gmail.com>
2016-07-20 10:12:01 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -14,9 +14,9 @@ LANGUAGE = 'en'
 NUM_PAGES = 15 
 
 for page in range(1, NUM_PAGES+1):
-    base_url = 'http://search.twitter.com/search.json?q=%s&geocode=%s&rpp=%s&lang=%s&page=%s' \
-         % (urllib2.quote(QUERY), urllib2.quote(GEOINFO), RESULTS_PER_PAGE, LANGUAGE, page)
-    try:
+    base_url = 'http://search.twitter.com/search.json?q=%s&rpp=%s&lang=%s&page=%s' \
+% (urllib2.quote(QUERY), RESULTS_PER_PAGE, LANGUAGE, page)
+       try:
         results_json = simplejson.loads(scraperwiki.scrape(base_url))
         for result in results_json['results']:
             data = {}
